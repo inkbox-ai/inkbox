e4793e58bab20f51f4050c011900e10987c01b8c
dimavrem22 <dimavrem22@gmail.com>
2026-09-22 06:54:58 +0000

    Release phone country metadata as 0.7.7

--- cli/src/client.ts
+++ cli/src/client.ts
@@ -6,7 +6,7 @@ import type { Command } from "commander";
 import { observeResponse } from "./response-metadata.js";
 
 // Keep in sync with package.json "version".
-export const CLI_VERSION = "0.7.6";
+export const CLI_VERSION = "0.7.7";
 
 export interface GlobalOpts {
   apiKey?: string;
